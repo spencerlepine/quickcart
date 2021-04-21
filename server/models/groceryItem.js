702edc9817cac1de15a6f89e975717d3497be2aa
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-04-21 11:37:06 -0400

    update schema

--- server/models/groceryItem.js
+++ server/models/groceryItem.js
@@ -4,8 +4,10 @@ const grocerySchema = mongoose.Schema({
     name: String,
     purchase_price: mongoose.Decimal128,
     purchase_size: String,
-    serving: String,
-    servings_per: Number,
+    serving_cost: mongoose.Decimal128,
+    category: String,
+    last_purchased: String,
+    priority: String,
 })
 
 const GroceryItem = mongoose.model('GroceryItem', grocerySchema)
