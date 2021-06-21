04a9c858958fae1280d5359e7f40c5525b737aa6
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-06-21 02:03:13 -0400

    fix mobile cart styles misalignment

--- src/components/CartLogPage/Receipt/CartLogItem/CartLogItem.js
+++ src/components/CartLogPage/Receipt/CartLogItem/CartLogItem.js
@@ -6,14 +6,13 @@ const CartLogItem = ({ cartLogItem }) => {
 
   const itemPrice = parseFloat(
     cartLogItem.purchase_price
-  )
-  const finalPrice = itemPrice * (cartLogItem.quantity || 1)
-  const priceString = finalPrice.toLocaleString("en-US", { style: "currency", currency: "USD" })
+  ).toFixed(2)
+  const finalPrice = Number.parseFloat(itemPrice * (cartLogItem.quantity || 1)).toFixed(2)
 
   return (
     <div className={classes.logView}>
       <h4 className={classes.productName}>{cartLogItem.name}</h4>
-      <h3 className={classes.productPrice}>{priceString}</h3>
+      <h3 className={classes.productPrice}>{finalPrice}</h3>
     </div>
   );
 };
