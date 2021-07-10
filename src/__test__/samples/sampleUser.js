2029a43eb58f5317c37c65d1bfd890404d5f36ef
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-10 12:17:40 -0700

    Remove project files that need to be refactored

--- src/__test__/samples/sampleUser.js
+++
@@ -1,7 +0,0 @@
-const mockCurrentUser = {
-    uid: "k3nQJ2a9fMcDIku6wBZObXMjpYo1",
-    displayName: "John Doe",
-    email: "johndoe@gmail.com",
-}
-
-export default mockCurrentUser
--- src/components/wrappers/Layout/styles.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export default makeStyles(() => ({
-  container: {
-    margin: "0.5em",
-    marginTop: "2em",
-  }
-}));
