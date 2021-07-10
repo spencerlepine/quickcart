ec95f5e50b39216a497a5fe2c336a39a3f8bb674
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-10 12:25:15 -0700

    add index.js file

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import MultiContextProvider from "./components/MultiContext/MultiContext";
+import './index.css';
+import App from './App';
+
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>
+    <MultiContextProvider>
+      <App />
+    </MultiContextProvider>
+  </Router >,
+  document.getElementById('root')
+);
