ae0e7463f1bc7db7058cd8be2e2b26b01abdbdc2
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-10 12:56:42 -0700

    add auth redirect hook to project

---
+++ src/hooks/useAuthRedirect/useAuthRedirect.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { WELCOME, HOME } from '../../constants/routeConstants';
+import useAuth from '../../context/AuthContext/AuthContext';
+import { Redirect } from 'react-router-dom';
+
+const withAuthRedirect = (Component, isAuthPage = false) => props => {
+  // Get the current user from AuthContext
+  const { currentUser } = useAuth();
+
+  if (currentUser) {
+    if (isAuthPage) {
+      return <Redirect to={HOME} />
+    } else {
+      return <Component />
+    }
+  } else {
+    if (isAuthPage) {
+      return <Component />
+    } else {
+      return <Redirect to={WELCOME} />
+    }
+  }
+}
+
+export default withAuthRedirect;
