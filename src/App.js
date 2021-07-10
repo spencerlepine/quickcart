a182155143a32629eac5e5f85c787c6a32ae2e94
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-10 12:35:39 -0700

    refactor app to use anonymous function convention

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Layout from "./components/wrappers/Layout/Layout"
-import Footer from "./components/Footer/Footer"
+import Layout from "./wrappers/Layout/Layout";
+import Footer from "./components/Footer/Footer";
 import Routes from "./components/pages/Routes/Routes";
-import NotificationsPopup from "./components/NotificationsPopup/NotificationsPopup"
+import NotificationsPopup from "./components/NotificationsPopup/NotificationsPopup";
 
-function App() {
-
-  return (
-    <>
-      <NotificationsPopup />
-      <Navbar />
-      <Layout>
-        <Routes />
-      </Layout>
-      <Footer />
-    </>
-  );
-}
+const App = () => (
+  <>
+    <NotificationsPopup />
+    <Navbar />
+    <Layout>
+      <Routes />
+    </Layout>
+    <Footer />
+  </>
+);
 
 export default App;
