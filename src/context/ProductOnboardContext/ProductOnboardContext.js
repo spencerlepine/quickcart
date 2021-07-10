d32162d3ef81e20b2fd16f47971050d55b7ba1b3
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-10 14:56:25 -0700

    add product onboarding context

---
+++ src/context/ProductOnboardContext/ProductOnboardContext.js
@@ -0,0 +1,53 @@
+import React, { useState, useContext } from 'react';
+import imageSearch from '../../modules/googleImageSearch';
+
+export const ProductOnboardContext = React.createContext();
+
+export function ProductOnboardProvider({ children }) {
+  const [loading, setLoading] = useState(false);
+  const [imageSearchResult, setImageSearchResult] = useState([]);
+
+  const searchCallback = (results) => {
+    const images = results.map(obj => {
+      return {
+        image: obj['image']['thumbnailLink'],
+        name: obj['title'],
+      };
+    });
+    setImageSearchResult(images);
+  }
+
+  const keywordSearch = async (keyword, callback) => {
+    setLoading(true);
+    await imageSearch(keyword, callback, 0, 5);
+    setLoading(false);
+  };
+
+  const value = {
+    loading,
+    imageSearchResult,
+    setImageSearchResult,
+    keywordSearch,
+    searchCallback,
+  };
+
+  return (
+    <ProductOnboardContext.Provider value={value}>
+      {children}
+    </ProductOnboardContext.Provider>
+  )
+}
+
+const useProductOnboard = () => {
+  const { imageSearchResult, setImageSearchResult, keywordSearch, loading, searchCallback } = useContext(ProductOnboardContext);
+
+  return {
+    loading,
+    imageSearchResult,
+    setImageSearchResult,
+    searchCallback,
+    keywordSearch,
+  };
+};
+
+export default useProductOnboard;
