c796c532b66f114e7d7474ae1d273a2ef511543e
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-14 16:23:39 -0700

    fix testing and webpack configuration

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), () => {
-  console.log('listening on port ', server.address().port);
+  console.log('Listening on port:', server.address().port);
 });
