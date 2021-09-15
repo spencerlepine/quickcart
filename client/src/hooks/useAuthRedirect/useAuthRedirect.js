64ad1303e775ecc7a7736d571aeac13d83573528
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-15 06:56:53 -0700

    create skeleton tests for useAuthRedirect hook

--- client/src/hooks/useAuthRedirect/useAuthRedirect.js
+++ client/src/hooks/useAuthRedirect/useAuthRedirect.js
@@ -3,25 +3,24 @@ import { WELCOME, HOME } from 'config/constants/routeConstants';
 import useAuth from 'context/AuthContext/AuthContext';
 import { Redirect } from 'react-router-dom';
 
-const withAuthRedirect =
-  (Component, isAuthPage = false) =>
-    () => {
+const withAuthRedirect = (Component, isAuthPage = false) =>
+  () => {
     // Get the current user from AuthContext
-      const { currentUser } = useAuth();
+    const { currentUser } = useAuth();
 
-      if (currentUser) {
-        if (isAuthPage) {
-          return <Redirect to={HOME} />;
-        } else {
-          return <Component />;
-        }
+    if (currentUser) {
+      if (isAuthPage) {
+        return <Redirect to={HOME} />;
       } else {
-        if (isAuthPage) {
-          return <Component />;
-        } else {
-          return <Redirect to={WELCOME} />;
-        }
+        return <Component />;
       }
-    };
+    } else {
+      if (isAuthPage) {
+        return <Component />;
+      } else {
+        return <Redirect to={WELCOME} />;
+      }
+    }
+  };
 
 export default withAuthRedirect;
