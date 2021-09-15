1fefb9eca1e666c2175ff7dc5893bf76cecfe971
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-15 05:40:51 -0700

    create mock productList object

---
+++ client/src/__mocks__/categorizedProductList.js
@@ -0,0 +1,75 @@
+const categorizedProductList = {
+  'frozenFood': {
+    'jJfndBeufiJEhgfu': {
+      '_id': 'jJfndBeufiJEhgfu',
+      'name': 'Pizza Rolls',
+      'purchase_size': {
+        'unit': 'oz',
+        'count': 24,
+      },
+      'purchase_price': 5.99,
+      'serving_size': {
+        'unit': 'oz',
+        'count': 3,
+      },
+      'servings_per': 8,
+      'brand': 'Tostino\'s',
+      'category': 'frozenFoods',
+    },
+  },
+  'meat': {
+    'jJfndBufiJEhgfu': {
+      '_id': 'jJfndBufiJEhgfu',
+      'name': 'Ground Beef',
+      'purchase_size': {
+        'unit': 'lbs',
+        'count': 1,
+      },
+      'purchase_price': 7.49,
+      'serving_size': {
+        'unit': 'oz',
+        'count': 3,
+      },
+      'servings_per': 3,
+      'category': 'frozenFoods',
+    },
+  },
+  'fruits': {
+    'jJfndfeufiJEhgfu': {
+      '_id': 'jJfndfeufiJEhgfu',
+      'name': 'Bananas',
+      'purchase_size': {
+        'unit': 'lbs',
+        'count': 1,
+      },
+      'purchase_price': 0.69,
+      'serving_size': {
+        'unit': 'grams',
+        'count': 118,
+      },
+      'servings_per': 1,
+      'category': 'fruits',
+    },
+  },
+  'vegetables': {},
+  'pantry': {
+    'jJfsadasufiJEhgfu': {
+      '_id': 'jJfsadasufiJEhgfu',
+      'name': 'Peanut Butter',
+      'purchase_size': {
+        'unit': 'oz',
+        'count': 16,
+      },
+      'purchase_price': 6.99,
+      'serving_size': {
+        'unit': 'oz',
+        'count': 10,
+      },
+      'servings_per': 15,
+      'brand': 'Jiffy',
+      'category': 'pantry',
+    },
+  },
+};
+
+export default categorizedProductList;
