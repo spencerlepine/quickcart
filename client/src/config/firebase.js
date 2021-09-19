cd5c355c736046139b02fc75101aa9172e467205
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-18 18:18:54 -0700

    mock the firebase for testing

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -2,7 +2,16 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/auth';
 import 'firebase/firestore';
-import { firebaseConfig } from 'config';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = firebase.initializeApp(firebaseConfig);
 export default app;
