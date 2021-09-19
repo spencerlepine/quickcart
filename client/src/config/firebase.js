c93c92157599dc7b42cf23ab7f0073815c69b05b
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-18 19:49:24 -0700

    account for missing env file

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -4,7 +4,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'placeholder',
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
