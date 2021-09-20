b42281dcd261626ac78e64cecb1578c3f17b363b
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-20 07:43:16 -0700

    serve static on fallback URL

--- server/index.js
+++ server/index.js
@@ -7,7 +7,16 @@ const buildOutputPath = path.join(__dirname, '../client', 'build');
 
 app.use(express.static(buildOutputPath));
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${PORT}`);
 });
